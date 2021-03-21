8c8e42ed5a4f432327b3e97e76140dcfb4adde12
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-03-20 23:53:57 -0400

    filter out array de timeslots

--- notaireApplicationFrontEnd/src/app/component/user/user.component.ts
+++ notaireApplicationFrontEnd/src/app/component/user/user.component.ts
@@ -3,7 +3,6 @@ import { RendezVousService } from './../../service/rendez-vous.service';
 import { XunkCalendarModule } from 'xunk-calendar';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { RendezVousDTO } from 'src/app/model/rendez-vous-dto';
-import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -19,12 +18,6 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     this.selDate = XunkCalendarModule.getToday();
     this.fetchAllRendezVous();
-    /*this.rendezVousService.fetchRendezVousById(1).subscribe((data)=>{
-      console.log(data);
-      console.log(Date.parse(data.dateDebut.toString()))
-      this.selDate=data.dateDebut;
-      console.log(this.selDate);
-    })*/
   }
   updateCurrentDate() {
     console.log(this.selDate)
@@ -43,33 +36,32 @@ export class UserComponent implements OnInit {
     this.timeSlots = this.timeSlots.filter(slot => this.checkIfTimeSlotAvalible(slot))
   }
   checkIfTimeSlotAvalible(timeSlot: TimeSlot): boolean {
+    let returnValue = true;
     this.rendezVousList.forEach(rv => {
-      if (this.isDateDebutRendezVousInTimeSlot(timeSlot,rv)||this.isDateFinRendezVousInTimeSlot(timeSlot,rv)) {
+      if (this.isDateDebutRendezVousInTimeSlot(timeSlot, rv) || this.isDateFinRendezVousInTimeSlot(timeSlot, rv)) {
         console.log("wapaww")
         console.log(timeSlot)
         console.log(rv);
-        return false
+        returnValue = false;
+
       }
     })
-    return true;
+    return returnValue;
   }
   initTimeSlots() {
     let i;
-    let compteurDheureDebut = 0;
-    let compteurDheureFin = 0;
+    let compteurDheure = 0;
     for (i = 0; i < 48; i = i + 1) {
-      let minutesDebut = 30;
-      let minutesFin = 0;
+      let demiHeure = 30;
+      let debutHeure = 0;
+      let timeSlot: TimeSlot = { dateDebut: this.initDateTimeSlot(compteurDheure, demiHeure), dateFin: this.initDateTimeSlot(compteurDheure, debutHeure) };
       if (!this.isOdd(i)) {
-        minutesDebut = 0;
-        minutesFin = 30;
+        timeSlot.dateDebut.setMinutes(debutHeure);
+        timeSlot.dateFin.setMinutes(demiHeure);
       }
-      let timeSlot: TimeSlot = { dateDebut: this.initDateTimeSlot(compteurDheureDebut, minutesDebut), dateFin: this.initDateTimeSlot(compteurDheureFin, minutesFin) };
       if (this.isOdd(i)) {
-        compteurDheureDebut = compteurDheureDebut + 1;
-      }
-      if (!this.isOdd(i)) {
-        compteurDheureFin = compteurDheureFin + 1;
+        timeSlot.dateFin.setHours(compteurDheure + 1);
+        compteurDheure = compteurDheure + 1;
       }
       this.timeSlots[i] = timeSlot;
     }
