dc535d9dccf22f6cc4975ae90b83fe9d2a28e288
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-03-28 17:07:58 -0400

    link from document preview to document view

--- notaireApplicationFrontEnd/src/app/component/fichier-document-preview/fichier-document-preview.component.ts
+++ notaireApplicationFrontEnd/src/app/component/fichier-document-preview/fichier-document-preview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FichierDocumentDTO } from 'src/app/model/fichier-document-dto';
 
 @Component({
   selector: 'app-fichier-document-preview',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class FichierDocumentPreviewComponent implements OnInit {
 
   constructor() { }
-
+  @Input() fichierDocument:FichierDocumentDTO
   ngOnInit(): void {
   }
 
