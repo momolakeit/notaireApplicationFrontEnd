17b0396b1f8b3f3b2b7d0093031bdb7d85aeae7e
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-04-02 19:43:41 -0400

    Update messaging.service.ts

--- notaireApplicationFrontEnd/src/app/service/messaging.service.ts
+++ notaireApplicationFrontEnd/src/app/service/messaging.service.ts
@@ -1,3 +1,4 @@
+import { MessagesDTO } from './../model/messages-dto';
 import { Stomp } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
 import { Injectable } from '@angular/core';
@@ -15,8 +16,16 @@ export class MessagingService {
     const ws = new SockJS(environment.baseUrl + "/addMessage");
                             
     this.stompClient = Stomp.over(ws);
+    const that = this;
     this.stompClient.connect({Authorization: "Bearer "+localStorage.getItem("token")}, function (frame) {
-
+      that.stompClient.subscribe("/conversation/1",(data)=>{
+          console.log("received")
+          console.log(data);
+      })
     })
+  }
+  sendMessage(){
+    var message : MessagesDTO ={id:null,user:null,conversation:null,message:"salut mon chummy"}
+    this.stompClient.send('/app/addMessage/1',null,JSON.stringify(message))
   }
 }
